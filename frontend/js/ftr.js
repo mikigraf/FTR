37c15151df2bdc3d5dd4a75b087bb663cb3cd6cb
Benjamin Awad <benm.awad@gmail.com>
2016-08-20 17:49:38 +0200

    Adds frontend file structure and simple web form

---
+++ frontend/js/ftr.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * FTR
+ *
+ */
+(function () {
+  'use strict';
+
+  function setHandlers() {
+    $('#main-form').submit(function (e) {
+      // Do AJAX fetching stuff in here.
+    });
+  }
+
+  setHandlers();
+
+})();
