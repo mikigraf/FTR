74aa3835b4eab02782a451ec54a2e77cb34dd0e1
Benjamin Awad <benm.awad@gmail.com>
2016-08-21 14:59:35 +0200

    Finishes Frontend (>implying...)

--- frontend/js/ftr.js
+++ frontend/js/ftr.js
@@ -91,42 +91,108 @@
   let resultDoodleView = {
     render: function (link) {
       return $('<div/>', {
-        'class': 'col-xs-4 col-md-3',
+        'class': 'col-xs-4 col-md-2 doodle-link',
       })
+      /*
       .append($('<a/>', {
         'href': '/doodleShare',
-        'text': 'Add'
-      }));
+      })
+      .append($('<button/>', {
+        'class': 'btn-default',
+        'text': 'Add to Doodle'
+      })));
+      */
+      .append($('<form/>')
+      .append($('<button/>', {
+        'id': 'addToDoodle',
+        'type': 'button',
+        'class': 'fixedbutton'
+      })));
+    }
+  }
+
+  let flightTableView = {
+    render: function(flights) {
+      let flightTableHeader = $('<thead/>')
+      .append($('<tr/>')
+      .append($('<th/>', {
+        'text': 'Start'
+      }))
+      .append($('<th/>', {
+        'text': 'Start Date/Time'
+      }))
+      .append($('<th/>', {
+        'text': 'Destination'
+      }))
+      .append($('<th/>', {
+        'text': 'Destination Time'
+      })));
+
+      let flightTableBody = $('<tbody/>')
+      .append($('<tr/>')
+      .append($('<td/>', {
+        'text': flights.from
+      }))
+      .append($('<td/>', {
+        'text': flights.departure
+      }))
+      .append($('<td/>', {
+        'text': flights.in
+      }))
+      .append($('<td/>', {
+        'text': flights.arrival
+      })));
+
+
+      let flightTable = $('<table/>', {
+        'class': 'table'
+      })
+      .append(flightTableHeader)
+      .append(flightTableBody);
+
+      return flightTable;
     }
   }
 
   let resultMainView = {
     render: function (hotel, flights) {
+      console.log(flights);
       return $('<div/>', {
-        'class': 'col-xs-8 col-md-6',
+        'class': 'col-xs-8 col-md-8',
         'text': hotel.name
-      });
+      })
+      .append('<p class="flight-label">Outward Flight</p>')
+      .append(flightTableView.render(flights.to))
+      .append('<p class="flight-label">Return Flight</p>')
+      .append(flightTableView.render(flights.back));
     }
   }
 
   let bookingView = {
     render: function (result) {
       return $('<div/>', {
-        'class': 'col-xs-4 col-md-3',
-        'text': 'Book now'
-      });
+        'class': 'col-xs-4 col-md-2',
+      })
+      .append($('<button/>', {
+        'class': 'btn-large',
+      })
+      .append($('<a/>', {
+        'href': '/asdf',
+        'text': 'Book now for only ' + result.price + '!'
+      })))
     }
   }
 
   let resultView = {
     render: function (result) {
+      console.log(result);
       let doodleView = resultDoodleView;
       let mainView = resultMainView;
       let elem = $('<div/>', {
         'class': 'row'
       })
       elem.append(doodleView.render(result.doodle))
-      elem.append(mainView.render(result.hotel));
+      elem.append(mainView.render(result.hotel, result.flights));
       elem.append(bookingView.render(result));
       return elem;
     }
